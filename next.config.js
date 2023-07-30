50523db40c30f66cb02e858edce8c72e56bc040e
Alvieto <haykalcs@gmail.com>
2023-07-30 12:46:13 +0700

    Update Headers Config

--- next.config.js
+++ next.config.js
@@ -14,6 +14,23 @@ const nextConfig = {
     loader: 'akamai',
     path: '',
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self' 'http://127.0.0.1:8000'",
+          },
+        ]
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
