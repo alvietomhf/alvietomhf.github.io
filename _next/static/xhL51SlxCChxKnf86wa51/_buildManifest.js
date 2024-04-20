9ced785958d0f780106a72651bb33b066ae1cc87
alvietomhf <haykalcs@gmail.com>
2024-04-20 09:37:19 +0000

    Deploying to gh-pages from @ alvietomhf/alvietomhf.github.io@bc3a75e7fef64ae076debc45e4cf09bc94caf348 🚀

--- _next/static/xhL51SlxCChxKnf86wa51/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-6c3db88e8bc52071.js","static/chunks/pages/index-411d2f2d8a0b5a74.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
